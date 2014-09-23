90b43423784b2acdc36aad88c99655289f3398d3
randyjacobus <rjacobus@asallc.com>
2014-09-22 22:16:51 -0500

    Update WNUS.R

--- WNUS.R
+++ WNUS.R
@@ -5,3 +5,14 @@ head(yields)
 sapply(yields,class)
 yields$Curve<-yields[,2]-yields[,3]
 head(yields)
+
+setwd("Users/rolandjacobus/Documents/Thesis")
+yields<-read.csv("Futures.csv")
+names(yields)<-c("DATE","T25","T15")
+yields$Curve<-yields[,2]-yields[,3]
+Diff<-diff(yields$Curve,lag=1)*100
+yields<-yields[-1,]
+yields$diff<-Diff
+head(yields)
+summary(yields$diff)
+hist(yields$diff)
